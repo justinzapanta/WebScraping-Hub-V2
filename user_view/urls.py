a01a51de5b679fb6a9a7efbc856d8fb183dff3e5
justinzapanta <zapantajustin5@gmail.com>
2024-04-13 22:28:31 +0800

    Sign In and Sign Up Done

--- user_view/urls.py
+++ user_view/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('sign-in/', views.sign_in, name='sign-in')
+    path('sign-in/', views.sign_in, name='sign-in'),
+    path('sign-up/', views.sign_up, name='sign-up'),
 ]
--- user_view/views.py
+++ user_view/views.py
@@ -1,10 +1,48 @@
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def index(request):
     return render(request, 'user_view/index.html')
 
 
+
 def sign_in(request):
-    return render(request, 'user_view/authentication/sign_in.html')
+    data = {'notif' : ''}
+
+    if request.method == 'POST':
+        user = authenticate(
+            username=request.POST['email'],
+            password=request.POST['password']
+        )
+
+        if user:
+            login(request, user)
+            return redirect('index') 
+        
+        data['notif'] = 'Invalid Email or Password'
+
+    return render(request, 'user_view/authentication/sign_in.html', data)
+
+
+
+def sign_up(request):
+    data = {'notif' : ''}
+
+    if request.method == 'POST':
+        user = User.objects.filter(
+            username=request.POST['email']
+        )
+            
+        if not user:
+            user = User.objects.create_user(
+                username=request.POST['email'],
+                password=request.POST['password'],
+                email=request.POST['email']
+            )
+            user.save()
+            return redirect('sign-in')
+
+        data['notif'] = 'Email Already Exists'
+    return render(request, 'user_view/authentication/sign_up.html', data)
